28dbba1e960a2413d36e6c2b04f006b3625aa23b
Cuong Nguyen <ntcuong2103@gmail.com>
2023-11-29 08:56:41 +0700

    added hidden single description

--- src/hidden_singles.h
+++ src/hidden_singles.h
@@ -8,6 +8,7 @@ typedef struct HiddenSingle_impl
     int value;
 } HiddenSingle;
 
+<<<<<<< HEAD
 // find hidden single values (the value that is unique among all the candidates for a row, a collumn, or a box)
 int find_hidden_single_values(Cell **p_cells, int *hidden_single_values);
 
@@ -19,3 +20,9 @@ void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles,
 int hidden_singles(SudokuBoard *p_board);
 bool is_in_Hidden(HiddenSingle*p_hidden_single, int *size,Cell*p );
 bool is_candidate(Cell *cell, int value);
+=======
+int find_hidden_single_values(Cell **p_cells, int *hidden_single_values);
+void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles,
+                        int *p_counter);
+int hidden_singles(SudokuBoard *p_board, Cell ***p_solved_cells);
+>>>>>>> efe80b5 (added hidden single description)
