8e5b808db29eb88520cd7b1e244d31f0de24adfe
Cuong Nguyen <ntcuong2103@gmail.com>
2023-11-30 08:15:22 +0700

    updated loop

--- src/sudoku.h
+++ src/sudoku.h
@@ -4,30 +4,18 @@
 
 #define BOARD_SIZE 9
 
-<<<<<<< HEAD
-struct Cell_impl
-{
-=======
 struct Cell_impl {
->>>>>>> efe80b5 (added hidden single description)
     int row_index;
     int col_index;
     int box_index;
     int num_candidates;
     int candidates[BOARD_SIZE];
-<<<<<<< HEAD
-    int value; // solved or 0
-    bool fixed;
-=======
     int value;  // solved or 0
->>>>>>> efe80b5 (added hidden single description)
 };
 
 typedef struct Cell_impl Cell;
 
-<<<<<<< HEAD
-struct SudokuBoard_impl
-{
+struct SudokuBoard_impl {
     int solved_counter;
     Cell **data; // 9x9 cell board
 
@@ -35,18 +23,7 @@ struct SudokuBoard_impl
     Cell **p_cols[BOARD_SIZE];  // cols pointers
     Cell **p_boxes[BOARD_SIZE]; // boxes pointers
     Cell *solved_cells[BOARD_SIZE *
-                       BOARD_SIZE]; // solved cell pointers (maximum)
-=======
-struct SudokuBoard_impl {
-    int solved_counter;
-    Cell **data;  // 9x9 cell board
-
-    Cell **p_rows[BOARD_SIZE];   // rows pointers
-    Cell **p_cols[BOARD_SIZE];   // cols pointers
-    Cell **p_boxes[BOARD_SIZE];  // boxes pointers
-    Cell *solved_cells[BOARD_SIZE *
                        BOARD_SIZE];  // solved cell pointers (maximum)
->>>>>>> efe80b5 (added hidden single description)
 };
 
 typedef struct SudokuBoard_impl SudokuBoard;
